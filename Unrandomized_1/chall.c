5c2eddc448e0255d433678d275c9712feeae5220
Younesfdj <ferradjiyounes69@gmail.com>
2024-07-05 00:10:43 +0100

    added guessing game 1

---
+++ Unrandomized_1/chall.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void flag(){
+    FILE* fd = fopen("flag.txt","r");
+
+    if (fd==NULL){
+        printf("Error while openning flag.txt\n") ;
+        exit(-1) ;
+    }
+
+    char flag[0x80];
+
+    fgets(flag,0x80,fd) ;
+
+    printf("%s",flag);
+
+    exit(0) ;
+}
+
+int main() {
+    srand(1337);
+    printf("Try to guess all the random numbers correctly and win a gift!\n");
+
+    for (int i = 0; i < 5; ++i) {
+        int random_number = rand() % 100;
+        int guess;
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        if (guess != random_number) {
+            printf("You failed! The number was %d\n", random_number);
+            return 0;
+        }
+    }
+    printf("You did it! Here is your gift: ");
+    flag();
+    return 0;
+}
